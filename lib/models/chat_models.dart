2b6d7fa2164844086e1b387e504f1fb6d7870bd2
SamilaChanu95 <chanukasamila@gmail.com>
2019-10-14 16:21:30 +0530

    Create different chat models with different users.

--- lib/models/chat_models.dart
+++ lib/models/chat_models.dart
@@ -17,6 +17,70 @@ class ChatModel
       message: "Created whatsapp UI.",
       time:"12.00 AM",
       avatarUrl: "https://www.facebook.com/photo.php?fbid=2398154430510448&set=a.1377014282624473&type=3&theater",
+    ),
+
+    new ChatModel
+    (
+      name: "Samila Chanuka",
+      message: "Creation of UI in whatsapp started.",
+      time:"09.00 AM",
+      avatarUrl: "https://www.facebook.com/photo.php?fbid=2398154430510448&set=a.1377014282624473&type=3&theater",
+    ),
+
+    new ChatModel
+    (
+      name: "Samila Chanuka",
+      message: "Created whatsapp UI pages.",
+      time:"11.30 AM",
+      avatarUrl: "https://www.facebook.com/photo.php?fbid=2398154430510448&set=a.1377014282624473&type=3&theater",
+    ),
+
+    new ChatModel
+    (
+      name: "Samila Chanuka",
+      message: "Created whatsapp UI chat models.",
+      time:"2.00 PM",
+      avatarUrl: "https://www.facebook.com/photo.php?fbid=2398154430510448&set=a.1377014282624473&type=3&theater",
+    ),
+
+    new ChatModel
+    (
+      name: "Samila Chanuka",
+      message: "Complete whatsapp UI.",
+      time:"4.00 PM",
+      avatarUrl: "https://www.facebook.com/photo.php?fbid=2398154430510448&set=a.1377014282624473&type=3&theater",
+    ),
+
+    new ChatModel
+    (
+      name: "Samila Chanuka",
+      message: "Created whatsapp UI.",
+      time:"12.00 AM",
+      avatarUrl: "https://www.facebook.com/photo.php?fbid=2398154430510448&set=a.1377014282624473&type=3&theater",
+    ),
+
+    new ChatModel
+    (
+      name: "Samila Chanuka",
+      message: "Created whatsapp UI.",
+      time:"1.00 AM",
+      avatarUrl: "https://www.facebook.com/photo.php?fbid=2398154430510448&set=a.1377014282624473&type=3&theater",
+    ),
+
+    new ChatModel
+    (
+      name: "Chanuka Samila", 
+      message: "Created whatsapp clone UI.",
+      time:"1.35 PM",
+      avatarUrl: "https://www.facebook.com/photo.php?fbid=2398154430510448&set=a.1377014282624473&type=3&theater",
+    ),
+
+    new ChatModel
+    (
+      name: "Samila Chanuka",
+      message: "Created whatsapp UI.",
+      time:"5.00 PM",
+      avatarUrl: "https://www.facebook.com/photo.php?fbid=2398154430510448&set=a.1377014282624473&type=3&theater",
     )
   ];
 
